3abf670fb567656641ac13ff8b3d16ff0cd230df
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-02-29 13:27:24 -0300

    service y interface para la comunicacion del chat

---
+++ src/app/interfaces/ChatMessageModel.ts
@@ -0,0 +1,4 @@
+export interface ChatMessage{
+    message: string,
+    username: string
+}
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { ChatMessage } from '../interfaces/ChatMessageModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
+  private stompClient : any;
+
   constructor() { }
+
+  initConnectionSocket(){
+    const url = '//localhost:300/chatsocket';
+
+    const socket = new SockJS(url);
+    
+    this.stompClient = Stomp.over(socket);
+  }
+
+
+  joinRoom(roomId: string){
+    this.stompClient.connect({}, () =>{
+      this.stompClient.suscribe(`/topic/${roomId}`, (messages: any) =>{
+        const messageContent = JSON.parse(messages.body);
+        console.log(messageContent);
+        
+      })
+    })
+  }
+
+  sendMessage(roomId: string, ChatMessage: ChatMessage){
+    this.stompClient.send(`/app/chat/${roomId}`, {}, JSON.stringify(ChatMessage));
+  }
 }

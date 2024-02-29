1eef9cb5ae6224bd1b457877e2818012fb989b86
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-02-29 13:36:16 -0300

    metodo para enviar msg

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { ChatMessage } from '../../interfaces/ChatMessageModel';
 
 @Component({
   selector: 'app-chat',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+
+  constructor(private chatService: ChatService){}
+
+  ngOnInit(): void{
+    this.chatService.joinRoom("sala-1");
+
+  }
+
+  sendMessageOnChat() {
+    const chatMessage = {
+      message: 'Prueba',
+      username: '1'
+    } as ChatMessage;
+    this.chatService.sendMessage("sala-1", chatMessage);
+  }
 
 }
